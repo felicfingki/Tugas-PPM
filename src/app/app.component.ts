f65df65193bfaedc8251899024b6770cacbc1c09
felicfingki <felicfingki@yahoo.com>
2020-04-16 20:25:35 +0700

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  
 })
 export class AppComponent  {
   name = 'Angular';
 
+  angka = document.getElementById("a1");
+  angkaa = document.getElementById("a2");
+  angka1=2;
+  angka2=3;
+  pangkat = Math.pow(this.angka1,this.angka2);
+
 }
+
+
+
